a52c802f7a3c7cdd20814c6de052958d8713cd1a
anwarjasim1 <154898447+anwarjasim1@users.noreply.github.com>
2026-02-28 04:38:30 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-function sayHello(){
-  alert("مرحبا! يمكنك التواصل معي عبر واتساب 😊");
-}
-
 const btn = document.getElementById("contactBtn");
 const menu = document.getElementById("contactDropdown");
 
-btn.addEventListener("click", () => {
-  menu.style.display = (menu.style.display === "block") ? "none" : "block";
-});
+if(btn && menu){
+  btn.addEventListener("click", () => {
+    menu.style.display = (menu.style.display === "block") ? "none" : "block";
+  });
 
-// إغلاق القائمة إذا ضغط المستخدم خارجها
-document.addEventListener("click", (e) => {
-  if (!e.target.closest(".contact-menu")) {
-    menu.style.display = "none";
-  }
-});
+  document.addEventListener("click", (e) => {
+    if (!e.target.closest(".contact-menu")) {
+      menu.style.display = "none";
+    }
+  });
+}
